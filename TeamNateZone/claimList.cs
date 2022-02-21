047c59ff92d897337798647b555dc5e06a7ea112
rmaier <rmaier@butler.edu>
2022-02-20 20:35:10 -0500

    updated view claim list for specific users

--- TeamNateZone/claimList.cs
+++ TeamNateZone/claimList.cs
@@ -36,6 +36,21 @@ namespace TeamNateZone
 
         private void claimList_Load(object sender, EventArgs e)
         {
+            SqlConnection cn = new SqlConnection();
+            SqlCommand cmd = new SqlCommand();
+            int account = user.getUserID();
+
+            cn.ConnectionString =
+                @"Data Source=se361.cysfo7qeek6c.us-east-1.rds.amazonaws.com;Initial Catalog=TEAM_A;Persist Security Info=True;User ID=TEAM_A;Password=j2uBr3v4F4y7kgAZF3CZmmMP;Encrypt=True;TrustServerCertificate=True";
+            cmd.Connection = cn;
+            cn.Open();
+            
+
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Claims WHERE UserID = @userid", cn);
+            da.SelectCommand.Parameters.AddWithValue("@userid", account);
+            DataTable dtbl = new DataTable();
+            da.Fill(dtbl);
+            dataGridView1.DataSource = dtbl;
             // TODO: This line of code loads data into the 'tEAM_A_Claims_Dataset.Claims' table. You can move, or remove it, as needed.
             //this.claimsTableAdapter.Fill(this.tEAM_A_Claims_Dataset.Claims);
 
