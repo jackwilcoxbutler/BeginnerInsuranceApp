7442475cd7d68c2230b730dff6dad465f0bca94b
Jack Wilcox <jrwilcox@butler.edu>
2022-04-14 11:27:01 -0400

    Fixed Admin View

--- TeamNateZone/AdminViewUsers.cs
+++ TeamNateZone/AdminViewUsers.cs
@@ -32,7 +32,7 @@ namespace TeamNateZone
                 @"Data Source=se361.cysfo7qeek6c.us-east-1.rds.amazonaws.com;Initial Catalog=TEAM_A;Persist Security Info=True;User ID=TEAM_A;Password=j2uBr3v4F4y7kgAZF3CZmmMP;Encrypt=True;TrustServerCertificate=True";
             cn.Open();
 
-            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM SignInInfo where clearance = 0 or clearance = 1 or clearance = 2 order by DateRegistered desc", cn);
+            SqlDataAdapter da = new SqlDataAdapter("SELECT Username,fName,lName,street,city,state,zip,clearance,UserID FROM SignInInfo where clearance = 0 or clearance = 1 or clearance = 2 order by DateRegistered desc", cn);
             DataTable dtbl = new DataTable();
             da.Fill(dtbl);
             dataGridView1.DataSource = dtbl;
