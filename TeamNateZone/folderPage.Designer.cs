140774c28cad4ee085eb6d224f48ed8ed757e07b
Jack Wilcox <jrwilcox@butler.edu>
2022-04-19 19:45:42 -0400

    UI Fixes

--- TeamNateZone/folderPage.Designer.cs
+++ TeamNateZone/folderPage.Designer.cs
@@ -30,6 +30,7 @@ namespace TeamNateZone
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(folderPage));
             this.panel1 = new System.Windows.Forms.Panel();
             this.btnBack = new CustomControls.RJControls.rndButton();
             this.dataGridView_Folders = new System.Windows.Forms.DataGridView();
@@ -55,9 +56,10 @@ namespace TeamNateZone
             // 
             this.panel1.Controls.Add(this.btnBack);
             this.panel1.Controls.Add(this.dataGridView_Folders);
-            this.panel1.Location = new System.Drawing.Point(2, 49);
+            this.panel1.Location = new System.Drawing.Point(4, 94);
+            this.panel1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(656, 402);
+            this.panel1.Size = new System.Drawing.Size(1312, 773);
             this.panel1.TabIndex = 0;
             // 
             // btnBack
@@ -71,10 +73,10 @@ namespace TeamNateZone
             this.btnBack.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnBack.ForeColor = System.Drawing.Color.White;
-            this.btnBack.Location = new System.Drawing.Point(543, 358);
-            this.btnBack.Margin = new System.Windows.Forms.Padding(2);
+            this.btnBack.Location = new System.Drawing.Point(1086, 688);
+            this.btnBack.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnBack.Name = "btnBack";
-            this.btnBack.Size = new System.Drawing.Size(94, 32);
+            this.btnBack.Size = new System.Drawing.Size(188, 62);
             this.btnBack.TabIndex = 35;
             this.btnBack.Text = "Back";
             this.btnBack.TextColor = System.Drawing.Color.White;
@@ -91,22 +93,28 @@ namespace TeamNateZone
             this.dataGridView_Folders.DataSource = this.foldersBindingSource;
             this.dataGridView_Folders.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dataGridView_Folders.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView_Folders.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.dataGridView_Folders.Name = "dataGridView_Folders";
-            this.dataGridView_Folders.Size = new System.Drawing.Size(656, 402);
+            this.dataGridView_Folders.RowHeadersWidth = 82;
+            this.dataGridView_Folders.Size = new System.Drawing.Size(1312, 773);
             this.dataGridView_Folders.TabIndex = 0;
             // 
             // folderIDDataGridViewTextBoxColumn
             // 
             this.folderIDDataGridViewTextBoxColumn.DataPropertyName = "FolderID";
             this.folderIDDataGridViewTextBoxColumn.HeaderText = "FolderID";
+            this.folderIDDataGridViewTextBoxColumn.MinimumWidth = 10;
             this.folderIDDataGridViewTextBoxColumn.Name = "folderIDDataGridViewTextBoxColumn";
             this.folderIDDataGridViewTextBoxColumn.ReadOnly = true;
+            this.folderIDDataGridViewTextBoxColumn.Width = 200;
             // 
             // userIDDataGridViewTextBoxColumn
             // 
             this.userIDDataGridViewTextBoxColumn.DataPropertyName = "UserID";
             this.userIDDataGridViewTextBoxColumn.HeaderText = "UserID";
+            this.userIDDataGridViewTextBoxColumn.MinimumWidth = 10;
             this.userIDDataGridViewTextBoxColumn.Name = "userIDDataGridViewTextBoxColumn";
+            this.userIDDataGridViewTextBoxColumn.Width = 200;
             // 
             // foldersBindingSource
             // 
@@ -125,9 +133,10 @@ namespace TeamNateZone
             this.panel2.Controls.Add(this.richTextBox1);
             this.panel2.Controls.Add(this.folderIdLbl);
             this.panel2.Controls.Add(this.folderIdTxt);
-            this.panel2.Location = new System.Drawing.Point(657, -2);
+            this.panel2.Location = new System.Drawing.Point(1314, -4);
+            this.panel2.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(143, 453);
+            this.panel2.Size = new System.Drawing.Size(286, 871);
             this.panel2.TabIndex = 1;
             // 
             // btnExpand
@@ -141,10 +150,10 @@ namespace TeamNateZone
             this.btnExpand.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnExpand.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnExpand.ForeColor = System.Drawing.Color.YellowGreen;
-            this.btnExpand.Location = new System.Drawing.Point(19, 334);
-            this.btnExpand.Margin = new System.Windows.Forms.Padding(2);
+            this.btnExpand.Location = new System.Drawing.Point(38, 642);
+            this.btnExpand.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnExpand.Name = "btnExpand";
-            this.btnExpand.Size = new System.Drawing.Size(112, 45);
+            this.btnExpand.Size = new System.Drawing.Size(224, 87);
             this.btnExpand.TabIndex = 35;
             this.btnExpand.Text = "Expand";
             this.btnExpand.TextColor = System.Drawing.Color.YellowGreen;
@@ -155,10 +164,11 @@ namespace TeamNateZone
             // 
             this.richTextBox1.BackColor = System.Drawing.Color.YellowGreen;
             this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.richTextBox1.Location = new System.Drawing.Point(18, 51);
+            this.richTextBox1.Location = new System.Drawing.Point(36, 98);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.ReadOnly = true;
-            this.richTextBox1.Size = new System.Drawing.Size(112, 96);
+            this.richTextBox1.Size = new System.Drawing.Size(224, 185);
             this.richTextBox1.TabIndex = 2;
             this.richTextBox1.Text = "Please type in the FolderID of the Client Folder you wish to look into in the tex" +
     "tbox below.";
@@ -166,25 +176,28 @@ namespace TeamNateZone
             // folderIdLbl
             // 
             this.folderIdLbl.AutoSize = true;
-            this.folderIdLbl.Location = new System.Drawing.Point(16, 182);
+            this.folderIdLbl.Location = new System.Drawing.Point(32, 350);
+            this.folderIdLbl.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.folderIdLbl.Name = "folderIdLbl";
-            this.folderIdLbl.Size = new System.Drawing.Size(53, 13);
+            this.folderIdLbl.Size = new System.Drawing.Size(105, 25);
             this.folderIdLbl.TabIndex = 1;
             this.folderIdLbl.Text = "Folder ID:";
             // 
             // folderIdTxt
             // 
-            this.folderIdTxt.Location = new System.Drawing.Point(19, 198);
+            this.folderIdTxt.Location = new System.Drawing.Point(38, 381);
+            this.folderIdTxt.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.folderIdTxt.Name = "folderIdTxt";
-            this.folderIdTxt.Size = new System.Drawing.Size(100, 20);
+            this.folderIdTxt.Size = new System.Drawing.Size(196, 31);
             this.folderIdTxt.TabIndex = 0;
             // 
             // panel3
             // 
             this.panel3.BackColor = System.Drawing.Color.YellowGreen;
-            this.panel3.Location = new System.Drawing.Point(2, -2);
+            this.panel3.Location = new System.Drawing.Point(4, -4);
+            this.panel3.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(656, 55);
+            this.panel3.Size = new System.Drawing.Size(1312, 106);
             this.panel3.TabIndex = 2;
             // 
             // foldersTableAdapter
@@ -193,12 +206,14 @@ namespace TeamNateZone
             // 
             // folderPage
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1600, 865);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "folderPage";
             this.Text = "folderPage";
             this.Load += new System.EventHandler(this.folderPage_Load);
